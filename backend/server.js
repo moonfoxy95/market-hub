7448c81567cff3be3fe21877c17ff538bb879d43
Vlad_workPC <moonfoxy@icloud.com>
2026-04-23 10:49:33 +0300

    change so requests from file:// dont block by cors

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+  origin: true,
+  cretdentials: true
 }));
 app.use(express.json());
-
 
 
 // ozon orders
