f243e905a29eafce7b4172cf67f90efc9e68d088
vlad laptop <moonfoxy@icloud.com>
2026-04-26 23:25:59 +0300

    add server & option to fetch from server for all markets

--- backend/script.js
+++ backend/script.js
@@ -1,23 +1,80 @@
 'use strict'
 
-let getOzonOrders = function () {
-  fetch('http://localhost:3000/api/ozon/unfulfilled-list', {
-    method: 'POST'
+const BACKEND_URL = 'http://localhost:3000';
+
+{
+  let getOzonOrdersButton = document.querySelector('#getOzonOrders');
+  let getYandexCampaignsButton = document.querySelector('#getYandexCampaigns');
+  let getYandexOrdersButton = document.querySelector('#getYandexOrders');
+  let getAliOrdersButton = document.querySelector('#getAliOrders');
+
+  getOzonOrdersButton.addEventListener('click', e => {
+    getOzonOrders();
+  });
+  getYandexCampaignsButton.addEventListener('click', e => {
+    getYandexCampaigns();
   })
-    .then(r => r.json())
-    .then(console.log);
+  getYandexOrdersButton.addEventListener('click', e => {
+    getYandexOrders();
+  })
+  getAliOrdersButton.addEventListener('click', e => {
+    getAliOrders();
+  })
+
+  let getOzonOrders = function () {
+    fetch(`${BACKEND_URL}/api/ozon/unfulfilled-list`, {
+      method: 'POST'
+    })
+      .then(r => r.json())
+      .then(console.log);
+  }
+
+  let getYandexCampaigns = function () {
+    fetch(`${BACKEND_URL}/api/yandex/campaigns`)
+      .then(r => r.json())
+      .then(console.log);
+  }
+
+  let getYandexOrders = function () {
+    fetch(`${BACKEND_URL}/api/yandex/orders`, {
+      method: 'POST'
+    })
+      .then(r => r.json())
+      .then(console.log);
+  }
+
+  let getAliOrders = function () {
+    fetch(`${BACKEND_URL}/api/ali/orders`, {
+      method: 'POST'
+    })
+      .then(response => response.json())
+      .then(console.log);
+  }
 }
 
-let getYandexCampaigns = function () {
-  fetch('http://localhost:3000/api/yandex/campaigns')
-    .then(r => r.json())
-    .then(console.log);
+{
+  async function checkServerStatus() {
+    try {
+      const response = await fetch(`${BACKEND_URL}/api/server-status`, {
+        method: 'GET',
+        cache: 'no-cache'
+      });
+      // сервер включен или ошибка метода
+      const statusText = response.ok ? '🟢' : `🔴 (${response.status}: ${response.statusText} ${response.url})`;
+
+      console.log(`Server: ${statusText}`);
+    } catch (error) { // сервер выключен
+      console.log(
+        `Server: 🔴 (${error.name}: ${error.message})`
+      );
+
+    }
+  }
+
+  // сразу первый раз
+  checkServerStatus();
+
+  // каждые 5 секунд
+  //setInterval(checkServerStatus, 5000);
 }
 
-let getYandexOrders = function () {
-  fetch('http://localhost:3000/api/yandex/orders', {
-    method: 'POST'
-  })
-    .then(r => r.json())
-    .then(console.log);
-}
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import keys from './keys.json' with { type: 'json' };
 
 const app = express();
 app.use(cors({
   origin: true,
-  cretdentials: true
+  credentials: true
 }));
 app.use(express.json());
 
+// health
+app.get('/api/server-status', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 // ozon orders
-app.post('/api/ozon/unfulfilled-list', async (req, res) => {
+app.post('/api/ozon/orders', async (req, res) => {
   try {
-    const OzonClientId = 36739;
-    const OzonApiKey = 'f6e35d96-e8a3-4f32-aacf-f86ce60ef9df';
     const time7DaysAgo = new Date(new Date().setDate(new Date().getDate() - 7)).toISOString();
     const time14DaysAhead = new Date(new Date().setDate(new Date().getDate() + 14)).toISOString();
 
@@ -42,8 +45,8 @@ app.post('/api/ozon/unfulfilled-list', async (req, res) => {
     const response = await fetch('https://api-seller.ozon.ru/v3/posting/fbs/unfulfilled/list', {
       method: 'POST',
       headers: {
-        'Client-Id': OzonClientId,
-        'Api-Key': OzonApiKey,
+        'Client-Id': keys.OZON_CLIENT_ID,
+        'Api-Key': keys.OZON_API_TOKEN,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
@@ -54,21 +57,19 @@ app.post('/api/ozon/unfulfilled-list', async (req, res) => {
     res.status(response.status).json(result);
   } catch (error) {
     res.status(500).json({
-      message: 'Server error',
+      message: 'Ошибка на сервере',
       error: String(error)
     });
   }
 });
 
-// yandex
+// yandex campaigns
 app.get('/api/yandex/campaigns', async (req, res) => {
   try {
-    let yandexKey = 'ACMA:ExCOgXcviQINftWJ33Aw7Y2XXB6FWIvX003eJvwm:1bf855e1';
-    let yandexCampaign = '21868557';
     const response = await fetch('https://api.partner.market.yandex.ru/v2/campaigns?limit=10&pageSize=1', {
       method: 'GET',
       headers: {
-        'Api-Key': yandexKey
+        'Api-Key': keys.YANDEX_API_TOKEN
       }
     });
 
@@ -86,9 +87,6 @@ app.get('/api/yandex/campaigns', async (req, res) => {
 // yandex orders
 app.post('/api/yandex/orders', async (req, res) => {
   try {
-    let yandexKey = 'ACMA:ExCOgXcviQINftWJ33Aw7Y2XXB6FWIvX003eJvwm:1bf855e1';
-    let yandexCampaign = '21868557';
-
     const data = {
       "programTypes": [
         "FBS"
@@ -107,10 +105,10 @@ app.post('/api/yandex/orders', async (req, res) => {
       ]
     };
 
-    let response = await fetch('https://api.partner.market.yandex.ru/v1/businesses/748599/orders', {
+    let response = await fetch(`https://api.partner.market.yandex.ru/v1/businesses/${keys.YANDEX_BUSINESS_ID}/orders`, {
       method: 'POST',
       headers: {
-        'Api-Key': yandexKey,
+        'Api-Key': keys.YANDEX_API_TOKEN,
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
@@ -128,66 +126,35 @@ app.post('/api/yandex/orders', async (req, res) => {
   }
 })
 
+// ali orders
+app.post('/api/ali/orders', async (req, res) => {
+  try {
+    //"delivery_statuses": ["Init"],
+    const data = {
+      "page_size": 100,
+      "page": 1
+    };
 
+    let response = await fetch('https://openapi.aliexpress.ru/seller-api/v1/order/get-order-list', {
+      method: 'POST',
+      headers: {
+        'x-auth-token': keys.ALI_API_TOKEN,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+    res.status(response.status).json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: 'Ошибка в получении запроса',
+      error: String(error)
+    });
+  }
+})
 
 app.listen(3000, () => {
   console.log('Server started on http://localhost:3000');
 });
-
-
-/* TESTS
-//yandex cabinets test
-let yandexKey = 'ACMA:ExCOgXcviQINftWJ33Aw7Y2XXB6FWIvX003eJvwm:1bf855e1';
-let yandexCampaign = '21868557';
-
-fetch('https://api.partner.market.yandex.ru/v2/campaigns?limit=10&pageSize=1', {
-  method: 'GET',
-  headers: {
-    'Api-Key': yandexKey
-  }
-})
-  .then(response => {
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Request failed: ', error));
-
-
-// yandex orders text
-let yandexKey = 'ACMA:ExCOgXcviQINftWJ33Aw7Y2XXB6FWIvX003eJvwm:1bf855e1';
-let yandexCampaign = '21868557';
-
-const data = {
-  "programTypes": [
-    "FBS"
-  ],
-  "statuses":
-    [
-      "PROCESSING"
-    ],
-  "substatuses":
-    [
-      "STARTED"
-    ],
-  "fake": false,
-  "sourcePlatforms": [
-    "MARKET"
-  ]
-};
-
-fetch('https://api.partner.market.yandex.ru/v1/businesses/748599/orders', {
-  method: 'POST',
-  headers: {
-    'Api-Key': yandexKey,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
-})
-  .then(response => {
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('Request failed: ', error));
-*/
